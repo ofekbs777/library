532cd5c1b8c3cf42cceffba801d9c85346ce6bdf
ofekbs777 <zzxxcc050@gmail.com>
2022-08-08 06:45:30 -0400

    f

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,12 +1,15 @@
-def call ()
-{
-Date date = new Date()
-String datePart = date.format("dd/MM/yyyy")
-String timePart = date.format("HH:mm:ss")
 
-println "datePart : " + datePart + "\ttimePart : " + timePart
-new File("/home/ubuntu/lib/").mkdir()
-}
+
+
+
+
+
+
+
+def today = new Date()
+def yesterday = today - 1
+println today.format("MM/dd/yyyy")
+println yesterday.format("MM/dd/yyyy")
 
 
 
